d9d0fb44204aa34726c00ae9a96bc019925b23ff
Jaydeboy <jn587256@gmail.com>
2018-04-22 20:46:42 -0400

    updated 2.1

--- src/main/java/edu/psu/ist440/team2/translator/LambdaFunctionHandler.java
+++ src/main/java/edu/psu/ist440/team2/translator/LambdaFunctionHandler.java
@@ -11,12 +11,15 @@ import java.io.UnsupportedEncodingException;
 
 import com.amazonaws.regions.Regions;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.model.GetObjectRequest;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3Object;
 import com.amazonaws.services.s3.model.S3ObjectInputStream;
+import com.amazonaws.auth.DefaultAWSCredentialsProviderChain;
+import com.darkprograms.speech.translator.GoogleTranslate; // repository of classes to connect to the google translation API
 
 /**
  * The Translator ...
@@ -24,25 +27,58 @@ import com.amazonaws.services.s3.model.S3ObjectInputStream;
  */
 public class LambdaFunctionHandler implements RequestHandler<DataModel, DataModel> {
 
+	StringBuilder sb = new StringBuilder();
+	
+
 	@Override
 	public DataModel handleRequest(DataModel input, Context context) {
 		context.getLogger().log("Input: " + input);
+		DataModel output = new DataModel(); 
+		// the holder for the translated data- text
+		String translated;
 
-		// TODO: implement your handler
+		try {
+			//retrieves the current bucket containing the text
+			String bucket = input.getDecryptedBucket();
+			//decryption key - name of file
+			String key = input.getDecryptedKey();
+			//targeted language used
+			String sourceLanguage = input.getSourceLanguage();
+			/*calls a static function to detect the language and translate it via google API (com.darkprograms.speech.translator.GoogleTranslate)
+				
+				*/
+			translated = GoogleTranslate.translate(sourceLanguage,  this.getDecryptedFileData(bucket, key));
+			this.writeTranslatedFile(bucket, key, translated);
+
+			context.getLogger().log("\nData: " +translated);
+			output.setConfidence(input.getConfidence());
+			output.setDecryptedBucket(input.getDecryptedBucket());
+			output.setDecryptedKey(input.getDecryptedKey());
+			output.setMethod(input.getMethod());
+			output.setSourceLanguage(input.getSourceLanguage());
+			output.setTranslatedBucket(input.getDecryptedBucket());
+			output.setTranslatedKey(input.getDecryptedKey());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		context.getLogger().log("Output: " + output);
 		return input;
 	}
 
 	/**
 	 * Gets the text of the decrypted source file
 	 * 
-	 * @param bucket name of the S3 bucket containing the decrypted file
-	 * @param key name of the decrypted file
+	 * @param bucket
+	 *            name of the S3 bucket containing the decrypted file
+	 * @param key
+	 *            name of the decrypted file
 	 * @return decrypted data as a string
-	 * @throws IOException if the decrypted data file cannot be read
+	 * @throws IOException
+	 *             if the decrypted data file cannot be read
 	 */
 	protected String getDecryptedFileData(String bucket, String key) throws IOException {
-		StringBuilder sb = new StringBuilder();
-
 		AmazonS3 s3client = AmazonS3ClientBuilder.standard().withRegion(Regions.US_EAST_1).build();
 		GetObjectRequest getRequest = new GetObjectRequest(bucket, key);
 		S3Object s3obj = s3client.getObject(getRequest);
@@ -67,17 +103,22 @@ public class LambdaFunctionHandler implements RequestHandler<DataModel, DataMode
 	/**
 	 * Writes the translation output to S3
 	 * 
-	 * @param bucket name of the S3 bucket to which the file will be saved
-	 * @param key file name
-	 * @param data a string containing the output of translation
+	 * @param bucket
+	 *            name of the S3 bucket to which the file will be saved
+	 * @param key
+	 *            file namexxxxx
+	 * @param data
+	 *            a string containing the output of translation
 	 * 
-	 * @throws UnsupportedEncodingException if the data string encoding is unsupported
+	 * @throws UnsupportedEncodingException
+	 *             if the data string encoding is unsupported
 	 */
 	protected void writeTranslatedFile(String bucket, String key, String data) {
 
 		byte[] dataByteArray = data.getBytes();
+
 		ByteArrayInputStream in = new ByteArrayInputStream(dataByteArray);
-		
+
 		ObjectMetadata metadata = new ObjectMetadata();
 		metadata.setContentType("text/plain");
 		metadata.setContentLength(dataByteArray.length);
